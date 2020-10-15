acf5337c6f1b60e18fb340055264ca63664961fd
José Francisco Jardim Fernandes <al218026@epcc.pt>
2020-10-15 15:38:45 +0100

    comeco do upload com form pelo code igniter

--- app2/application/controllers/Upload.php
+++ app2/application/controllers/Upload.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Upload extends CI_Controller {
+        /*
+        Formulario, em que nao sera utilizado HTML, mas sim os metodos do helper From. Ad tags HTMK relativas ao formulario, como form , input, textarea, e label, passarao a metodos do helper From.
+        As tags <form> e </form> serao substituidas pelos metodos form_open_multipart() e form_close()
+        Iremos fazer envio de arquivos e precisam que o entype do formulario seja multipart/form-data
+        <input> por form_input(), etc....
+        */
         public function __construct(){
                 parent::__construct();
                 $this->load->helper('url');  
